99fc2422473b19ad74dd54b35e48c9c326f0d83b
itcast <itheima@itcast.cn>
2021-03-14 13:13:06 +0800

    完成用户列表

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 Vue.use(Router)
 const router = new Router({ // eslint-disable-line no-unused-vars
     routes: [
         { path: '/', redirect: '/login' },
         { path: '/login', component: Login },
-        { path: '/home', component: Home }
+        { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }] }
     ]
 })
 router.beforeEach((to, form, next) => {
