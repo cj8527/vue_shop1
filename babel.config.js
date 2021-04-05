56928594826ce6b17b665ad74e29222004f6d169
itcast <itheima@itcast.cn>
2021-04-05 18:36:54 +0800

    111

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
+const prodPlugins = []
+if(process.env.NODE_ENV === 'production') {
+  rodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/app'
   ],
   plugins: [
     [
@@ -9,6 +13,7 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    ...prodPlugins
   ]
 }
--- src/main.js
+++ src/main.js
@@ -12,12 +12,19 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
+    NProgress.start()
     console.log(config)
     config.headers.Authorization = window.sessionStorage.getItem('token')
     return config
+})
+axios.interceptors.response.use(config => {
+    NProgress.done()
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
